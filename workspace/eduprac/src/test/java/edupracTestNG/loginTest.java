8dbf8687fa194e2e87ef1e5560d80129c8894f64
ubuntu <mounamukhar@gmail.com>
2019-11-18 01:27:44 +0000

    Selenium test script

---
+++ workspace/eduprac/src/test/java/edupracTestNG/loginTest.java
@@ -0,0 +1,23 @@
+package edupracTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class loginTest {
+	
+	@Test
+	public static void test1() throws InterruptedException{
+		
+		//System.setProperty("webdriver.firefox.marionette","/home/ubuntu/edureka_practice/SeleniumSite/drivers/geckodriver");
+		//WebDriver driver = new FirefoxDriver();
+ 	
+        String baseUrl = "http://localhost:3001/";
+        //driver.quit();
+		//String baseUrl = "http://www.yahoo.com";
+        System.out.println("Selenium script executed!");
+		
+	}
+
+}
